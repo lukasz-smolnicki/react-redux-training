592bedfcd284ae7dbd12134422d7d53685d895ec
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-03-01 21:45:04 +0100

    part 3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {
+  Provider
+} from 'react-redux';
+import {
+  createStore, applyMiddleware
+} from 'redux';
 
 import rootReducer from './reducers/index'
 import App from './App';
 
-const store = createStore(rootReducer);
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispaching', action)
+  let result = next(action);
+  console.log('next state', store.getState())
+  console.groupEnd()
+  return result
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(<React.StrictMode>
+  <Provider store={store} >
+    <App />
+  </Provider>
+</React.StrictMode>, document.getElementById('root'));
